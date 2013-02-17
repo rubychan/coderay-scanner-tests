c1580c7d9631a95f9cc8f224e0d144dbd5b67bab
Kornelius Kalnbach <murphy@rubychan.de>
2013-02-17 20:51:08 +0100

    update/extend Ruby tests

--- ruby/ruby19.in.rb
+++ ruby/ruby19.in.rb
@@ -1,1 +1,19 @@
 block.(*arguments)  # bovi's example
+
+new = { hash: :syntax }
+call(with, hash: :syntax)
+this_crazy(hash: :syntax)
+test hash: :syntax
+test \
+  hash: :syntax,
+  multi: :line
+
+more hash::tests
+more hash:"tests"
+more hash:42
+more hash:??
+more hash:{}
+more hash:nil
+more hash:foo
+more hash://
+_ _:_
--- ruby/unicode.in.rb
+++ ruby/unicode.in.rb
@@ -28,3 +28,5 @@ class << Osaka = Object.new
     sleep ∞
   end
 end
+
+?× != :∑∞∑
